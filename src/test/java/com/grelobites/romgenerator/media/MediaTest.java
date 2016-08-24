5f1f911bbd2649b728606a45a114a2dbd1f01e2b
mteira <manuel.teira@gmail.com>
2016-08-24 12:54:39 +0200

    Add wav generation test suite

---
+++ src/test/java/com/grelobites/romgenerator/media/MediaTest.java
@@ -0,0 +1,46 @@
+package com.grelobites.romgenerator.media;
+
+import com.grelobites.romgenerator.util.Util;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class MediaTest extends Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MediaTest.class);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        File file = new File("/Users/mteira/Desktop/test.wav");
+        FileOutputStream fos = new FileOutputStream(file);
+        CompressedWavOutputStream wos = new CompressedWavOutputStream(fos, WavOutputFormat.defaultDataFormat());
+        wos.write(Util.fromInputStream(new FileInputStream("/Users/mteira/Desktop/dandanator-mini.rom")));
+        wos.close();
+        fos.close();
+        LOGGER.debug("Creating media from " + file.toURI().toURL().toExternalForm());
+        Media media = new Media(file.toURI().toURL().toExternalForm());
+        MediaPlayer player = new MediaPlayer(media);
+        player.setAutoPlay(true);
+        MediaView mediaView = new MediaView(player);
+        AnchorPane pane = new AnchorPane();
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setScene(scene);
+        pane.getChildren().add(mediaView);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
---
+++ src/test/java/com/grelobites/romgenerator/media/WavOutputFormat.java
@@ -0,0 +1,159 @@
+package com.grelobites.romgenerator.media;
+
+public class WavOutputFormat {
+    public static final int SRATE_44100 = 44100;
+    public static final int SRATE_48000 = 48000;
+    public static final int HEADER_FLAG_BYTE = 0;
+    public static final int DATA_FLAG_BYTE = 0xff;
+
+    public static final int DEFAULT_PILOT_DURATION = 1000;
+    public static final int DEFAULT_PAUSE_DURATION = 1000;
+    public static final int DEFAULT_SPEED = 3;
+    public static final int DEFAULT_OFFSET = 0;
+    private int sampleRate;
+    private ChannelType channelType;
+    private int flagByte;
+    private int speed;
+    private int offset;
+    private int pilotDurationMillis;
+    private int finalPauseDurationMillis;
+
+    public static class Builder {
+        private WavOutputFormat outputFormat = new WavOutputFormat();
+
+        public Builder withSampleRate(int sampleRate) {
+            outputFormat.setSampleRate(sampleRate);
+            return this;
+        }
+
+        public Builder withChannelType(ChannelType channelType) {
+            outputFormat.setChannelType(channelType);
+            return this;
+        }
+
+        public Builder withFlagByte(int flagByte) {
+            outputFormat.setFlagByte(flagByte);
+            return this;
+        }
+
+        public Builder withSpeed(int speed) {
+            outputFormat.setSpeed(speed);
+            return this;
+        }
+
+        public Builder withOffset(int offset) {
+            outputFormat.setOffset(offset);
+            return this;
+        }
+
+        public Builder withPilotDurationMillis(int pilotDurationMillis) {
+            outputFormat.setPilotDurationMillis(pilotDurationMillis);
+            return this;
+        }
+
+        public Builder withFinalPauseDurationMillis(int finalPauseDurationMillis) {
+            outputFormat.setFinalPauseDurationMillis(finalPauseDurationMillis);
+            return this;
+        }
+
+        public WavOutputFormat build() {
+            return outputFormat;
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static WavOutputFormat defaultHeaderFormat() {
+        return new Builder().withSampleRate(SRATE_44100)
+                .withChannelType(ChannelType.MONO)
+                .withFlagByte(HEADER_FLAG_BYTE)
+                .withSpeed(DEFAULT_SPEED)
+                .withOffset(DEFAULT_OFFSET)
+                .withPilotDurationMillis(DEFAULT_PILOT_DURATION)
+                .withFinalPauseDurationMillis(DEFAULT_PAUSE_DURATION)
+                .build();
+    }
+
+    public static WavOutputFormat defaultDataFormat() {
+        return new Builder().withSampleRate(SRATE_44100)
+                .withChannelType(ChannelType.MONO)
+                .withFlagByte(DATA_FLAG_BYTE)
+                .withSpeed(DEFAULT_SPEED)
+                .withOffset(DEFAULT_OFFSET)
+                .withPilotDurationMillis(DEFAULT_PILOT_DURATION)
+                .withFinalPauseDurationMillis(DEFAULT_PAUSE_DURATION)
+                .build();
+    }
+
+    public WavOutputFormat(int sampleRate, ChannelType channelType, int flagByte,
+                           int speed, int offset, int pilotDurationMillis,
+                           int finalPauseDurationMillis) {
+        this.sampleRate = sampleRate;
+        this.channelType = channelType;
+        this.flagByte = flagByte;
+        this.speed = speed;
+        this.offset = offset;
+        this.pilotDurationMillis = pilotDurationMillis;
+        this.finalPauseDurationMillis = finalPauseDurationMillis;
+    }
+
+    public WavOutputFormat() {}
+
+    public int getSampleRate() {
+        return sampleRate;
+    }
+
+    public void setSampleRate(int sampleRate) {
+        this.sampleRate = sampleRate;
+    }
+
+    public ChannelType getChannelType() {
+        return channelType;
+    }
+
+    public void setChannelType(ChannelType channelType) {
+        this.channelType = channelType;
+    }
+
+    public int getFlagByte() {
+        return flagByte;
+    }
+
+    public void setFlagByte(int flagByte) {
+        this.flagByte = flagByte;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getPilotDurationMillis() {
+        return pilotDurationMillis;
+    }
+
+    public void setPilotDurationMillis(int pilotDurationMillis) {
+        this.pilotDurationMillis = pilotDurationMillis;
+    }
+
+    public int getFinalPauseDurationMillis() {
+        return finalPauseDurationMillis;
+    }
+
+    public void setFinalPauseDurationMillis(int finalPauseDurationMillis) {
+        this.finalPauseDurationMillis = finalPauseDurationMillis;
+    }
+}
