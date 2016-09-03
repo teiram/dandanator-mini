b3ef1c417754549c215a300788926327cdcac360
mteira <manuel.teira@gmail.com>
2016-09-03 22:01:24 +0200

    Preparing for version 5.1

--- src/main/java/com/grelobites/romgenerator/Constants.java
+++ src/main/java/com/grelobites/romgenerator/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     public static final String ROMSET_PROVIDED = "__ROMSET_PROVIDED__";
     public static final int SLOT_SIZE = 0x4000;
 
-    private static final String DEFAULT_VERSION = "5.0";
+    private static final String DEFAULT_VERSION = "5.1";
 
     public static final int CHARSET_SIZE = 768;
 
