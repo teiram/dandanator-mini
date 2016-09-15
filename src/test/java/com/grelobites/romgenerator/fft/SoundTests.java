7a32403a5cecc8b4e3ce5de0c4644e7c5cca2dbd
mteira <manuel.teira@gmail.com>
2016-09-15 11:25:06 +0200

    Renamed to SoundTests to avoid maven to execute them

--- src/test/java/com/grelobites/romgenerator/fft/SoundTests.java
+++ src/test/java/com/grelobites/romgenerator/fft/SoundTests.java
@@ -12,12 +12,12 @@ import javax.sound.sampled.TargetDataLine;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-public class SoundTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SoundTest.class);
+public class SoundTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SoundTests.class);
 
     @Test
     public void testSineWave() throws Exception {
-        ToneInputStream is = new ToneInputStream(44100.0f, AudioSystem.NOT_SPECIFIED, 1.0, 1000.0);
+        ToneInputStream is = new ToneInputStream(44100.0f, AudioSystem.NOT_SPECIFIED, 1.0, 9900.0);
         SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, is
                 .getFormat(), ((int) is.getFrameLength()));
         SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
@@ -46,6 +46,7 @@ public class SoundTest {
         }
         Complex[] result = FFT.fft(source);
         double maxValue = 0.0;
+        double minValue = Double.MAX_VALUE;
         int freqIndex = 0;
         for (int j = 0; j <= result.length / 2; j++) {
             double value = result[j].abs();
@@ -53,7 +54,13 @@ public class SoundTest {
                 maxValue = value;
                 freqIndex = j;
             }
+            if (value < minValue) {
+                minValue = value;
+            }
         }
+
+        LOGGER.debug("DC value is " + result[0].abs() + ", max value ratio is " + maxValue / minValue
+                + ", DC ratio is " + maxValue / result[0].abs());
         return freqIndex;
     }
 
@@ -70,7 +77,7 @@ public class SoundTest {
         thread.start();
 
         float sampleRate = 44100.0f;
-        int fftSize = 1024;
+        int fftSize = 1024; // 1024 temporal samples mean 1024 / 44100 seconds = 23.21 ms
         TargetDataLine line;
         AudioFormat format = //new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
                //44100.0f , 16, 1, 2, 44100.0f, false);
