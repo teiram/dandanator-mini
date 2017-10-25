27f1760c4a0d1f8f56d0e8e4a42fb878f652313a
mteira <manuel.teira@gmail.com>
2017-10-25 10:35:15 +0200

    Do not reset the rom byte array when the romsetpath is set to ROM provided

--- src/main/java/com/grelobites/romgenerator/handlers/dandanatormini/DandanatorMiniConfiguration.java
+++ src/main/java/com/grelobites/romgenerator/handlers/dandanatormini/DandanatorMiniConfiguration.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.prefs.Preferences;
 
 public class DandanatorMiniConfiguration {
@@ -134,7 +135,9 @@ public class DandanatorMiniConfiguration {
     }
 
     public void setExtraRomPath(String extraRomPath) {
-        extraRom = null;
+        if (!Constants.ROMSET_PROVIDED.equals(extraRomPath)) {
+            extraRom = null;
+        }
         this.extraRomPath.set(extraRomPath);
     }
 
@@ -290,6 +293,24 @@ public class DandanatorMiniConfiguration {
             }
         }
         return dandanatorPicFirmware;
+    }
+
+    @Override
+    public String toString() {
+        return "DandanatorMiniConfiguration{" +
+                "dandanatorRomPath=" + dandanatorRomPath +
+                ", extraRomPath=" + extraRomPath +
+                ", dandanatorPicFirmwarePath=" + dandanatorPicFirmwarePath +
+                ", togglePokesMessage=" + togglePokesMessage +
+                ", extraRomMessage=" + extraRomMessage +
+                ", launchGameMessage=" + launchGameMessage +
+                ", selectPokesMessage=" + selectPokesMessage +
+                ", disableBorderEffect=" + disableBorderEffect +
+                ", autoboot=" + autoboot +
+                ", dandanatorRom=" + dandanatorRom +
+                ", extraRom=" + extraRom +
+                ", dandanatorPicFirmware=" + dandanatorPicFirmware +
+                '}';
     }
 
     private static DandanatorMiniConfiguration setFromPreferences(DandanatorMiniConfiguration configuration) {
