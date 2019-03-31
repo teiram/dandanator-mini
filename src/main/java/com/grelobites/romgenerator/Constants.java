875a3f28d04409b73536c226f1494497942b33c2
Manuel Teira <manuel.teira@gmail.com>
2019-03-31 11:10:55 +0200

    Update default version (for dev runs)

--- src/main/java/com/grelobites/romgenerator/Constants.java
+++ src/main/java/com/grelobites/romgenerator/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public static final int SECTOR_SIZE = 0x1000;
     public static final int SLOT_SIZE = 0x4000;
 
-    private static final String DEFAULT_VERSION = "8.0";
+    private static final String DEFAULT_VERSION = "8.1";
 
     public static final int CHARSET_SIZE = 768;
 
